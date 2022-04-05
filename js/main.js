e16347dfec27a9d0422c01156dd1fd233e96d4a3
Ezabulbary <ezabul.bary@gmail.com>
2022-04-05 15:34:37 +0600

    display phone

--- js/main.js
+++ js/main.js
@@ -16,3 +16,28 @@ const searchPhone = () => {
     }
 }
 
+const displayPhone = (phones) => {
+    const displaContainar = document.getElementById('displa-containar');
+    displaContainar.textContent = '';
+    if (phones.length == 0) {
+        const keywordError = document.getElementById('keyword-error');
+        keywordError.style.display = 'block';
+        const inputError = document.getElementById('input-error');
+        inputError.style.display = 'none';
+    }
+    phones.forEach(phone => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card p-4 rounded-3">
+            <img src="${phone.image}" class="card-img-top h-50" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">${phone.brand}</p>
+            </div>
+            <button onclick="loadDetails(${phone.slug})" class="mx-auto w-50 rounded-3">Details</button>
+        </div>
+        `;
+        displaContainar.appendChild(div);
+    })
+}
